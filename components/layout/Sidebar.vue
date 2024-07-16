31726c486b71566437a16291f480a14b5514cf7b
krasilnikovWeb <antoniys009@gmail.com>
2024-07-16 22:10:46 +0700

    add nuxt image, tailwind css, shadcn

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/">
+      <NuxtImg src="/logo.svg" alt="" width="100px" />
+    </NuxtLink>
+    <UiButton />
+  </aside>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
