f4877672e7bad7d6f481643e03b4d79dcf98ec0e
krasilnikovWeb <antoniys009@gmail.com>
2024-07-16 23:48:06 +0700

    shadcn init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt"]
+  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
 });
