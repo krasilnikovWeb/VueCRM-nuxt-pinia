7904b8b1c1960019c26babd549067569246da12c
krasilnikovWeb <antoniys009@gmail.com>
2024-08-04 13:12:50 +0700

    add shadcn CardUI and install DayJS

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System</h1>
+    <div>
+      <Card class="mb-5" draggable="true">
+        <CardHeader role="button"> name card </CardHeader>
+        <CardContent> Company </CardContent>
+        <CardFooter> Date </CardFooter>
+      </Card>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useSeoMeta({
+  title: "Home | CRM System",
+});
+</script>
 
 <style></style>
