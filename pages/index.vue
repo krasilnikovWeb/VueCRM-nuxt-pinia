2b6b2fcebbb70c8789dc371c833801942e9cc5a8
krasilnikovWeb <antoniys009@gmail.com>
2024-08-08 17:14:24 +0700

    add gardient style

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,14 @@
         <div
           v-for="(column, index) in data"
           :key="column.id"
+          @dragover="handleDragOver"
+          @drop="() => handleDrop(column)"
           class="min-h-screen"
         >
-          <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+          <div
+            class="rounded bg-slate-700 py-1 px-5 mb-2 text-center"
+            :style="generateColumnStyle(index, data?.length)"
+          >
             {{ column.name }}
           </div>
           <KanbanCreateDeal :refetch="refetch" :status="column.id" />
@@ -18,6 +23,7 @@
             :key="card.id"
             class="mb-5"
             draggable="true"
+            @dragstart="() => handleDragStart(card, column)"
           >
             <CardHeader role="button">
               <CardTitle>{{ card.name }}</CardTitle>
@@ -43,6 +49,10 @@
 import type { ICard, IColumn } from "~/components/kanban/kanban.types";
 import { useKanbanQuery } from "~/components/kanban/useKanbanQuery";
 import dayjs from "dayjs";
+import { useMutation } from "@tanstack/vue-query";
+import type { EnumStatus } from "~/types/deals.types";
+import { COLLECTION_DEALS, DB_ID } from "~/app.constants";
+import { generateColumnStyle } from "@/components/kanban/generate-gradient";
 
 useSeoMeta({
   title: "Home | CRM System",
@@ -50,6 +60,37 @@ useSeoMeta({
 
 const dragCardRef = ref<ICard | null>(null);
 const sourceColumnRef = ref<IColumn | null>(null);
+
+type TypeMutationVariables = {
+  docId: string;
+  status?: EnumStatus;
+};
+
+const { mutate } = useMutation({
+  mutationKey: ["move card"],
+  mutationFn: ({ docId, status }: TypeMutationVariables) =>
+    DB.updateDocument(DB_ID, COLLECTION_DEALS, docId, {
+      status,
+    }),
+  onSuccess: () => {
+    refetch();
+  },
+});
+
+function handleDragStart(card: ICard, column: IColumn) {
+  dragCardRef.value = card;
+  sourceColumnRef.value = column;
+}
+
+function handleDragOver(event: DragEvent) {
+  event.preventDefault();
+}
+
+function handleDrop(targetColumn: IColumn) {
+  if (dragCardRef.value && sourceColumnRef.value) {
+    mutate({ docId: dragCardRef.value.id, status: targetColumn.id });
+  }
+}
 
 const { data, isLoading, refetch } = useKanbanQuery();
 /* const store = useDealSlideStore() */
